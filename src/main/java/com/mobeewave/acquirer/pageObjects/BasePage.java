cd8a94cad6b0f75d37882cda2d7aaae759bf8ae3
shalikaE <shalika.e@sl.aeturnum.com>
2019-03-12 15:49:12 +0530

    Updating Merchant Page

--- src/main/java/com/mobeewave/acquirer/pageObjects/BasePage.java
+++ src/main/java/com/mobeewave/acquirer/pageObjects/BasePage.java
@@ -20,25 +20,27 @@ public class BasePage {
 	public static String lbl_merchantId_xpath = "//select[@id='filterChoice']";
 	public static String tf_merchantfilterchoice_xpath = "//input[@id='filterByFieldValue']";
 	public static String lbl_perpageCount_number_xpath = "//button[@id='dropdownMenu1";
-	public static String drpdn_perpageCount_xpath ="//div[@class='dropdown open']";
-	public static String lbl_perpageCount_xpath= "//span[@class='dropdown-label']";
+	public static String drpdn_perpageCount_xpath = "//div[@class='dropdown open']";
+	public static String lbl_perpageCount_xpath = "//span[@class='dropdown-label']";
 	public static String tf_merchatId_Partialxpath = "//td[contains(text(),'";
 	public static String tf_merchantName_xpath = "//td[contains(text(),'Simon Says')]";
-	public static String merchantRecord_xpath = "//td[contains(text(),'Simon Says')]/.."; //need to implement 
+	public static String merchantRecord_xpath = "//td[contains(text(),'Simon Says')]/.."; // need to implement
 	public static String btn_span_xpath = "//span[contains(text(),'«')]";
-	//======= Details popup
+	// ======= Details popup
 	public static String btn_resendActivation_mail_xpath = "//button[contains(text(),'Resend Activation Email')]";
-	public static String btn_lock_xpath ="//button[@class='btn btn-primary btn-sm pull-right']";
+	public static String btn_lock_xpath = "//button[@class='btn btn-primary btn-sm pull-right']";
 	public static String btn_unlocked_xapth = "//button[contains(text(),'LOCK')]";
-	public static String lbl_panel_generalTitle_xpath = "//h3[contains(text(),'General Information')]";//need to implement table 
-	public static String lbl_merchantName_xpath ="//h4[contains(text(),'Doing Business As (Merchant Name')]";
-	public static String lbl_registrationDate_xpath ="//h4[contains(text(),'Registration Date')]";
-	public static String lbl_address_xpath ="//h4[contains(text(),'Address (Street and Number)')]";
-	public static String lbl_city_xpath ="//h4[contains(text(),'City')]";
-	public static String lbl_country_xpath ="//h4[contains(text(),'Country')]";
-	public static String lbl_provinceState_xpath ="//h4[contains(text(),'Province/State')]";
-	public static String lbl_zipcode_xpath ="//h4[contains(text(),'Zip Code')]";
-	public static String lbl_merchantTimeZone_xpath ="//h4[contains(text(),'Merchant Time Zone')]";
+	public static String lbl_panel_generalTitle_xpath = "//h3[contains(text(),'General Information')]";// need to
+																										// implement
+																										// table
+	public static String lbl_merchantName_xpath = "//h4[contains(text(),'Doing Business As (Merchant Name')]";
+	public static String lbl_registrationDate_xpath = "//h4[contains(text(),'Registration Date')]";
+	public static String lbl_address_xpath = "//h4[contains(text(),'Address (Street and Number)')]";
+	public static String lbl_city_xpath = "//h4[contains(text(),'City')]";
+	public static String lbl_country_xpath = "//h4[contains(text(),'Country')]";
+	public static String lbl_provinceState_xpath = "//h4[contains(text(),'Province/State')]";
+	public static String lbl_zipcode_xpath = "//h4[contains(text(),'Zip Code')]";
+	public static String lbl_merchantTimeZone_xpath = "//h4[contains(text(),'Merchant Time Zone')]";
 	public static String lbl_preferredCommunicationLanguage_xpath = "//h4[contains(text(),'Preferred Communication Language')]";
 	public static String lbl_panel_mpos_title_xpath = "//h3[contains(text(),'Mpos Administrator')]";
 	public static String lbl_fullName_xpath = "//h4[contains(text(),'Merchant Full Name')]";
@@ -49,25 +51,32 @@ public class BasePage {
 	public static String lbl_currencyDefenition_xpath = "//div[@id='ppinfoCBA']//*[contains(text(),'Currency Definition')]";
 	public static String lbl_panel_pendingterminaTitle_xpath = "//h3[contains(text(),'Pending Terminals'')]";
 	public static String lbl_terminalIdentifier_xpath = "//div[@class='panel panel-default']//div[@id='globalPending']//th[contains(text(),'Terminal Identifier')]";
-	public static String lbl_option_xpath ="//div[@class='panel panel-default']//div[@id='globalPending']//th[contains(text(),'Options')]";
-	public static String lbl_panel_activeterminalsTitle_xpath ="[contains(text(),'Active Terminals')]";
+	public static String lbl_option_xpath = "//div[@class='panel panel-default']//div[@id='globalPending']//th[contains(text(),'Options')]";
+	public static String lbl_panel_activeterminalsTitle_xpath = "[contains(text(),'Active Terminals')]";
 	public static String lbl_terminalIdentifier_partialXpath = " //div[@id='active']//th[contains(text(),'Terminal Identifier')]";
 	public static String lbl_terminalName = "//div[@class='panel panel-default']//th[contains(text(),'Terminal Name')]";
 	public static String lbl_safetyNetAttestationStatus_partialXpath = " //div[@class='panel panel-default']//th[contains(text(),'SafetyNet Attestation Status')]";
 	public static String lbl_option_partialXpath = " //div[@class='panel panel-default']//div[@id='active']//th[contains(text(),'Options')]";
-	public static String btn_ellipsis_xpath = "//i[@class='mfb-component__main-icon--active fa fa-ellipsis-v']"; 
+	public static String btn_ellipsis_xpath = "//i[@class='mfb-component__main-icon--active fa fa-ellipsis-v']";
 	public static String btn_editMerchanDetails_xpath = "]"; // need to implement
-	public static String lbl_editGeneralInformation_xpath ="//*[contains(text(),'Edit General Information')]";
-	public static String lbl_businessInformation_xpath ="//*[contains(text(),'Business Information')]";
-
+	public static String lbl_editGeneralInformation_xpath = "//*[contains(text(),'Edit General Information')]";
+	public static String lbl_businessInformation_xpath = "//*[contains(text(),'Business Information')]";// need to add feilds
+	public static String tf_generalEdit_merchantName_xpath = "//input[@id='name'] ";
+	public static String tf_generalAddresss_StreetAndNumber_xpath = "//label[contains(text(),'Address (Street and Number)')]/following-sibling::*"; //reference -need to complete from grid 
+	public static String btn_deleteMerchant_xpath = "//i[@class='mfb-component__child-icon fa fa-trash']";
+	public static String btn_ok_xpath = "//button[contains(text(),'Ok')]";
+	public static String btn_cancel_xpath = "//button[contains(text(),'Cancel')]";
+	public static String lbl_confermationNeeded_xpath = "//*[contains(text(),'Confirmation Needed')]";
+	public static String lbl_areyouSure_txt_xpath = "//*[contains(text(),'Are you sure?')]";
 	
+
 	// Transaction_Page Identifiers
 
 	// AuditLog_Page Identifiers
 
 	public static void areElementsOveralapped() {
 		CommonUtils.checkOverlap();
-		CommonUtils.makeBothElementListandMapClear(); 
+		CommonUtils.makeBothElementListandMapClear();
 	}
 
 	public static void getAllPageEliments() {
