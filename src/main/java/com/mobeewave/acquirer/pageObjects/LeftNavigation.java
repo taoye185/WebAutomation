6b748c4d6e98db028fa1c78ddd41aeaf8cc47ded
Kpmgithub <madurajith.p@aeturnum.com>
2019-03-19 15:19:53 +0530

    Identify left navigation bottom elements

--- src/main/java/com/mobeewave/acquirer/pageObjects/LeftNavigation.java
+++ src/main/java/com/mobeewave/acquirer/pageObjects/LeftNavigation.java
@@ -7,22 +7,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import com.mobeewave.acquirer.utils.CommonUtils;
 
+// TODO: Auto-generated Javadoc
 /**
- * 
- * @author madurajith.p
+ * The Class LeftNavigation.
  *
+ * @author madurajith.p
  */
 public class LeftNavigation extends BasePage {
+
 /**
- * 
- * @param leftnavItem, in run time this will refer final strings mentioned under Left Navigation in Base Page
+ * The user profile.
+ *
  * @return left navigation link
  */
+	public static WebElement userProfile;
 	
+	/** The pin. */
+	public static WebElement pin;
 	
+	/** The logout. */
+	public static WebElement logout;
+	
+	/**
+	 * Left navigation link.
+	 *
+	 * @param leftnavItem the leftnav item
+	 * @return the web element
+	 */
 	public WebElement leftNavigationLink(String leftnavItem) {
 		String leftnavlink = BasePage.lnk_partial_LeftNav_xpath + leftnavItem + "')]";
 		return CommonUtils.IsElementSelectable(By.xpath(leftnavlink), 20);
 	}
 
+	
+	/**
+	 * Sets the left navigation bottom elements.
+	 */
+	public static void setLeftNavBottomElements(){
+		WebElement parent = CommonUtils.IsElementSelectable(By.xpath(parentlefnav_xpath), 20);
+		List<ArrayList> listtemp = CommonUtils.getChildElements(parent);
+		 userProfile = (WebElement) listtemp.get(25);
+		 pin = (WebElement) listtemp.get(27);
+		 logout = (WebElement) listtemp.get(29);	
+	}
 }
