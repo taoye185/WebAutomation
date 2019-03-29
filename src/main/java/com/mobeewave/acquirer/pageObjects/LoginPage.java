12ca068183102bf1aa9e4d6d2039b4649cac9128
Kpmgithub <madurajith.p@aeturnum.com>
2019-03-29 16:35:52 +0530

    code review changes

--- src/main/java/com/mobeewave/acquirer/pageObjects/LoginPage.java
+++ src/main/java/com/mobeewave/acquirer/pageObjects/LoginPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import com.mobeewave.acquirer.utils.CommonUtils;
 
 
-
 /// <summary>
 
 /// The LoginPage class.
@@ -14,34 +13,32 @@ import com.mobeewave.acquirer.utils.CommonUtils;
 /// </summary>
 
 public class LoginPage extends BasePage {
-
+	WebElement usernameTxtBox;
+	WebElement passwordTxtBox;
+	WebElement signInBtn;
+	WebElement forgetPasswordLink;
+	
 	// region PageElements
 
 	public WebElement usernameTxtBox() {
-		WebElement usernameTxtBox = CommonUtils.IsElementSelectable(By.xpath(BasePage.tf_username_xpath), "LoaginPage.UserName");
-		// ElementList.add(usernameTxtBox);
-		//addElementstoMap(usernameTxtBox, "LoaginPage.UserName");
-		return usernameTxtBox;
+		return	usernameTxtBox = CommonUtils.IsElementSelectable(By.xpath(BasePage.tf_username_xpath), "LoginPage.UserName");
+			 
 	}
 
 	public WebElement passwordTxtBox() {
-		WebElement passwordTxtBox = CommonUtils.IsElementSelectable(By.xpath(BasePage.tf_password_xpath), "LoginPage.Password");
-	//	addElementstoMap(passwordTxtBox, "LoginPage.Password");
-		return passwordTxtBox;
+		return	passwordTxtBox = CommonUtils.IsElementSelectable(By.xpath(BasePage.tf_password_xpath), "LoginPage.Password");
+	
 	}
 
 
 	public WebElement signInBtn() {
-		WebElement signInBtn = CommonUtils.IsElementSelectable(By.xpath(BasePage.btn_signIn_xpath), "LoginPage.SignInBtn");
-	//	addElementstoMap(signInBtn, "LoginPage.SignInBtn");		
-		return signInBtn;
+		return signInBtn = CommonUtils.IsElementSelectable(By.xpath(BasePage.btn_signIn_xpath), "LoginPage.SignInBtn");
+	
 	}
 	
 	public WebElement forgetPasswordLink() {
-		WebElement forgetPasswordLink = CommonUtils.IsElementSelectable(By.xpath(BasePage.lnk_forgetPassword_xpath),
+		return forgetPasswordLink = CommonUtils.IsElementSelectable(By.xpath(BasePage.lnk_forgetPassword_xpath),
 				"LoginPage.ForgetPasswordLink");
-		//addElementstoMap(forgetPasswordLink, "LoginPage.ForgetPasswordLink");
-		return forgetPasswordLink;
 	}
 
 
