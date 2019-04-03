142699413303af48049f92c6924ac7a4cd5e4161
Kpmgithub <madurajith.p@aeturnum.com>
2019-04-03 16:24:15 +0530

    Created Test Runners for Chrome and Firefox

---
+++ src/test/java/com/mobeewave/acquirer/testRunner/RunFireFoxTest.java
@@ -0,0 +1,27 @@
+package com.mobeewave.acquirer.testRunner;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.mobeewave.acquirer.infastructure.SetUp;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber-html-report",
+		"json:target/cucumber-report.json" }, glue = "com.mobeewave.acquirer.tests", 
+		features = "src/test/features", tags={"@sanity3"})
+
+public class RunFireFoxTest {
+
+	/// <summary>
+	/// This method can be use to set initial background before test starts
+	/// </summary>
+	/// <returns></returns>
+
+	@BeforeClass
+	public static void init() {
+		SetUp.setupBrowser("firefox");
+	}
+}
